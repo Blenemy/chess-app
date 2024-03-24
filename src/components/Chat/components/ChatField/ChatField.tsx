de0fd65c5ca6c87d73cf4dc5e71e5d80f59d0da3
Denys Topchyi <miliarder97@gmail.com>
2024-03-24 22:40:38 +0200

    feat: finished styles for chat

--- src/components/Chat/components/ChatField/ChatField.tsx
+++ src/components/Chat/components/ChatField/ChatField.tsx
@@ -1,10 +1,36 @@
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
+import cn from "classnames";
 
 import "./ChatField.scss";
 
+const CHAT_PRESETS = [
+  {
+    ["data-title"]: "Hello!",
+    message: "Hi",
+  },
+  {
+    ["data-title"]: "Good luck!",
+    message: "GL",
+  },
+  {
+    ["data-title"]: "Have fun!",
+    message: "hf",
+  },
+  {
+    ["data-title"]: "You too!",
+    message: "u2",
+  },
+];
+
 const ChatField = () => {
   const [messages, setMessages] = useState<string[]>([]);
+  const [currentMessage, setCurrentMessage] = useState<string>("");
+  const [disabledPresets, setDisabledPresets] = useState<string[]>([]);
+
+  const isDisabledPreset = (preset: string) => {
+    return disabledPresets.some((el) => el === preset);
+  };
 
   const socket = io(`wss://langcards.fun`, {
     transports: ["websocket", "polling"],
@@ -30,6 +56,13 @@ const ChatField = () => {
   const sendMessage = (message: string) => {
     socket.emit("message", message);
   };
+  const handleOnEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" && e.currentTarget.value) {
+      const message = e.currentTarget.value;
+      sendMessage(message);
+      e.currentTarget.value = "";
+    }
+  };
 
   return (
     <div className="group-chat">
@@ -44,14 +77,27 @@ const ChatField = () => {
         type="text"
         className="group-chat__input"
         placeholder="Будьте вежливы в чате!"
-        onKeyDown={(e) => {
-          if (e.key === "Enter" && e.currentTarget.value) {
-            const message = e.currentTarget.value;
-            sendMessage(message);
-            e.currentTarget.value = "";
-          }
-        }}
+        onKeyDown={handleOnEnter}
+        value={currentMessage}
+        onChange={(e) => setCurrentMessage(e.target.value)}
       />
+      <div className="group-chat__presets">
+        {CHAT_PRESETS.map((preset) => (
+          <article
+            key={preset["data-title"]}
+            className={cn("group-chat__title", {
+              "disabled-preset": isDisabledPreset(preset.message),
+            })}
+            data-title={preset["data-title"]}
+            onClick={() => {
+              sendMessage(preset["data-title"]);
+              setDisabledPresets((prev) => [...prev, preset.message]);
+            }}
+          >
+            {preset.message}
+          </article>
+        ))}
+      </div>
     </div>
   );
 };

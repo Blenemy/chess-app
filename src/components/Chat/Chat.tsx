fa815a699cbe99d15a590c9153d822adf0cd3b86
Denys Topchyi <miliarder97@gmail.com>
2024-03-24 22:48:39 +0200

    feat: add box shadow to the chat, refactored tabs rendering

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -7,8 +7,8 @@ import Notes from "./components/Notes/Notes";
 import "./Chat.scss";
 
 const tabs = [
-  { label: "Chat", component: <ChatField /> },
-  { label: "Notes", component: <Notes /> },
+  { label: "Чат", component: <ChatField /> },
+  { label: "Заметки", component: <Notes /> },
 ];
 
 const Chat = () => {
@@ -18,28 +18,20 @@ const Chat = () => {
   return (
     <section className="chat">
       <div className="chat__tabs">
-        <div
-          onClick={() => {
-            setCurrentTabIndex(0);
-          }}
-          className={cn("chat__tab", {
-            "active-tab": currentTabIndex === 0,
-            "not-active-tab": currentTabIndex !== 0,
-          })}
-        >
-          Чат
-        </div>
-        <div
-          onClick={() => {
-            setCurrentTabIndex(1);
-          }}
-          className={cn("chat__tab", {
-            "active-tab": currentTabIndex === 1,
-            "not-active-tab": currentTabIndex !== 1,
-          })}
-        >
-          Заметки
-        </div>
+        {tabs.map((tab, index) => (
+          <div
+            onClick={() => {
+              setCurrentTabIndex(index);
+            }}
+            className={cn("chat__tab", {
+              "active-tab": currentTabIndex === index,
+              "not-active-tab": currentTabIndex !== index,
+            })}
+            key={tab.label}
+          >
+            {tab.label}
+          </div>
+        ))}
       </div>
       <div className="chat__content">{currentTab.component}</div>
     </section>
